8ec3d4414638949537ae77508abf81bf8d5346ec
Anton Orlichenko <aorliche@gmail.com>
2024-01-24 19:03:41 -0600

    Added old GAN code

---
+++ static/js/main.js
@@ -0,0 +1,39 @@
+const $ = q => document.querySelector(q);
+const $$ = q => [...document.querySelectorAll(q)];
+
+window.addEventListener('load', () => {
+    $('#number').addEventListener('input', () => {
+        let val = $('#number').value;
+        val = 10 ** parseFloat(val);
+        $('#number-value').innerText = Math.round(val);
+    });
+    $('#age').addEventListener('input', () => {
+        let val = $('#age').value;
+        $('#age-value').innerText = parseInt(val);
+    });
+    $('#preset').addEventListener('change', () => {
+        const idx = $('#preset').selectedIndex;
+        const val = $('#preset').options[idx].value;
+        let [age, sex, race, task] = val.split('-');
+        age = parseInt(age);
+        $('#age').value = age;
+        $('#age-value').innerText = age;
+        if (sex == 'male') {
+            $('#male').checked = true;
+        } else {
+            $('#female').checked = true;
+        }
+        for (let i=0; i<$('#race').options.length; i++) {
+            if ($('#race').options[i].value == race) {
+                $('#race').selectedIndex = i;
+                break;
+            }
+        }
+        for (let i=0; i<$('#task').options.length; i++) {
+            if ($('#task').options[i].value == task) {
+                $('#task').selectedIndex = i;
+                break;
+            }
+        }
+    });
+});
