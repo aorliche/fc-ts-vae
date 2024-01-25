8d69474116e19d2baeed54123b09507de4445d1e
Anton Orlichenko <aorliche@gmail.com>
2024-01-25 16:04:52 -0600

    Changed port

--- server/app.py
+++ server/app.py
@@ -126,4 +126,4 @@ def difference():
     return render_template('image.html', img=img)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8009, debug=True, threaded=True)
+    app.run(host='0.0.0.0', port=8010, debug=True, threaded=True)
